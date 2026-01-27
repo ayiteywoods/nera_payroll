6b1653de066c0a1f9f206f3e8668160ecfb508ba
James Abeo <jamesabeojr@gmail.com>
2026-01-27 11:33:22 +0000

    Second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+
+
+import { redirect } from "next/navigation";
+import React from "react";
+
 
 export default function Home() {
+  redirect ("/login")
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-5xl">Welcome to Nera Payroll</h1>
-    </div>
+  <> </>
   );
 }
